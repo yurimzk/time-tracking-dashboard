f0d2b07ed1ee7f4913cae146da722a1d6a265fef
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-02-23 20:44:10 -0300

    set up and config tailwindcss and sass

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.export = {
+  content: ["./*.html", "./src/**/*.{html,js,scss}"],
+  theme: {
+    extend: {
+      colors: {
+        'user-blue': 'hsl(246, 80%, 60%)',
+        'light-red-work': 'hsl(15, 100%, 70%)',
+        'soft-blue-play': 'hsl(195, 74%, 62%)',
+        'light-red-study': 'hsl(348, 100%, 68%)',
+        'lime-green-exercise': 'hsl(145, 58%, 55%)',
+        'violet-social': 'hsl(264, 64%, 52%)',
+        'soft-orange-self-care': 'hsl(43, 84%, 65%)',
+        'very-dark-blue': 'hsl(226, 43%, 10%)',
+        'dark-blue': 'hsl(235, 46%, 20%)',
+        'desatured-blue': 'hsl(235, 45%, 61%)',
+        'pale-blue': 'hsl(236, 100%, 87%)',
+        'white': 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
