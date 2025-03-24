1f47876559d5d504476f70353ec71d1a7e2c14d1
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-24 16:46:11 -0300

    update dom with daily, weekly and monthly data

--- src/main.js
+++ src/main.js
@@ -1,29 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const daily = document.querySelector('.daily');
-  const weekly = document.querySelector('.weekly');
-  const monthly = document.querySelector('.monthly');
+document.addEventListener('DOMContentLoaded', function () {
+  const dailyBtn = document.querySelector('.daily-btn')
+  const weeklyBtn = document.querySelector('.weekly-btn')
+  const monthlyBtn = document.querySelector('.monthly-btn')
 
-  let json;
+  let json
 
-  fetch('data.json').then((response) => {
-    if (!response.ok)
-      throw new Error('Something went wrong!');
+  fetch('data.json')
+    .then((response) => {
+      if (!response.ok) throw new Error('Something went wrong!')
 
-      return response.json();
-  }).then((data) => {
-    json = data;
+      return response.json()
+    })
+    .then((data) => {
+      json = data
+    })
+
+  function updateDOM(timeframe, currentData, previousData) {
+    const categories = ['work', 'play', 'study', 'exercise', 'social', 'self-care'];
+
+    categories.forEach((category, index) => {
+      document.querySelector(`.current-${category}-day`).innerHTML = `${currentData[index]}hrs`;
+
+      let previousLabel;
+      if (timeframe === 'daily') {
+        previousLabel = 'Last Day';
+      } else if (timeframe === 'weekly') {
+        previousLabel = 'Last Week';
+      } else if (timeframe === 'monthly') {
+        previousLabel = 'Last Month';
+      }
+
+      document.querySelector(`.previous-${category}-day`).innerHTML = `${previousLabel} - ${previousData[index]}hrs`;
+    });
+  }
+
+  function handleTimeframe(timeframe) {
+    if (json) {
+      const currentData = [];
+      const previousData = [];
+
+      json.forEach(item_ => {
+        currentData.push(item_.timeframes[timeframe].current);
+        previousData.push(item_.timeframes[timeframe].previous);
+      })
+      updateDOM(timeframe,currentData, previousData);
+    }
+  }
+
+  dailyBtn.addEventListener('click', function () {
+    handleTimeframe('daily');
   })
 
-
-  daily.addEventListener('click', function() {
-    console.log('testing');
+  weeklyBtn.addEventListener('click', function () {
+    handleTimeframe('weekly');
   })
 
-  weekly.addEventListener('click', function() {
-    console.log('testing 1');
-  })
-
-  monthly.addEventListener('click', function() {
-    console.log('testing 2');
+  monthlyBtn.addEventListener('click', function () {
+    handleTimeframe('monthly');
   })
 })
